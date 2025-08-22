c83ea68d349a4860fa3c92a5ce1bb38f3ac06628
segfaultscribe <segfaultscribe@gmail.com>
2025-08-22 23:53:51 +0530

    add block headers

--- baby_malloc/bby_malloc.c
+++ baby_malloc/bby_malloc.c
@@ -1,15 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define POOL_SIZE 1024*1024
 
 static char memory_pool[POOL_SIZE];
 static size_t offset = 0;
 
+
+static block_meta* free_list = (block_meta*)memory_pool;
+
 typedef struct {
     size_t size;      
     int free;         
     struct block_meta* next; 
 } block_meta;
+
+void internal_allocation(size_t size){
+    block_meta *curr = free_list;
+    size = (size + 7) & ~7;
+
+    while(curr){
+        if(curr->free && curr->size > size){
+            curr->free = 0;
+
+            if(curr->size > size + sizeof(block_meta)){
+                block_meta *next = (block_meta*)((char*)curr + sizeof(block_meta) + size);
+
+                next->size = curr->size - size - sizeof(block_meta);
+                next->free = 1;
+                next->next = curr->next;
+
+                curr->size = size;
+                curr->next = next;
+            }
+
+            return (char*)curr + sizeof(block_meta);
+        }
+        curr = curr->next;
+    }
+    return NULL;
+}
 
 void *bby_malloc(void *pointer, size_t size){
     int check = offset + size < POOL_SIZE ? 1 : 0;
@@ -22,5 +52,5 @@ void *bby_malloc(void *pointer, size_t size){
 }
 
 int main(){
-    
+
 }
