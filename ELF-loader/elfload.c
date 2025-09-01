7678e9f7db4bfc5fb98e592e02764c8542ce2434
segfaultscribe <segfaultscribe@gmail.com>
2025-09-01 23:58:52 +0530

    add elf validation

--- ELF-loader/elfload.c
+++ ELF-loader/elfload.c
@@ -20,5 +20,22 @@ int main(int argc, char *argv[]){
         return 1;
     }
 
+    if (elf_header.e_ident[EI_MAG0] != ELFMAG0 ||
+        elf_header.e_ident[EI_MAG1] != ELFMAG1 ||
+        elf_header.e_ident[EI_MAG2] != ELFMAG2 ||
+        elf_header.e_ident[EI_MAG3] != ELFMAG3) {
+        fprintf(stderr, "Not a valid ELF file\n");
+        close(fd);
+        return 1;
+    }
+
+    printf("Valid ELF file detected!\n");
+    printf("Entry point: 0x%lx\n", elf_header.e_entry);
+    printf("Program header offset: %lu\n", elf_header.e_phoff);
+    printf("Section header offset: %lu\n", elf_header.e_shoff);
+
+    close(fd);
+    return 0;
+
 
 }
