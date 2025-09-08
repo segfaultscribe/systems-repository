54c22aefd33a1338eef34fb0c179ffa1dd58c413
segfaultscribe <segfaultscribe@gmail.com>
2025-09-08 23:52:04 +0530

    modularised the code into functions

--- toy-shell/carapace.c
+++ toy-shell/carapace.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <signal.h>
 
 #define MAX_BUFFER_SIZE 256
 #define MAX_TOKENS 50
@@ -12,60 +13,79 @@ void handle_sigint(int sig) {
     fflush(stdout);
 }
 
-int main(){
+void print_prompt() {
+    printf("carapace> ");
+    fflush(stdout);
+}
+
+int parse_input(char *input, char **tokens, int *is_background_process) {
+    int tokenCount = 0;
+    // Remove trailing newline replace it with '\0'
+    input[strcspn(input, "\n")] = '\0';
+    char *word = strtok(input, " ");
+    while (word != NULL && tokenCount < MAX_TOKENS - 1) {
+        tokens[tokenCount] = word;
+        tokenCount++;
+        word = strtok(NULL, " ");
+    }
+    tokens[tokenCount] = NULL;
+    // Check for background process indicated by '&'
+    if (tokenCount > 0 && strcmp(tokens[tokenCount - 1], "&") == 0) {
+        *is_background_process = 1;
+        tokens[tokenCount - 1] = NULL;
+        tokenCount--;
+    } else {
+        *is_background_process = 0;
+    }
+    return tokenCount;
+}
+
+void execute_command(char **tokens, int is_background_process) {
+    pid_t pid = fork();
+    if (pid == -1) {
+        perror("Fork failed:");
+    } else if (pid == 0) {
+        // Child process
+        if (execvp(tokens[0], tokens) == -1) {
+            perror("execvp failed");
+        }
+        _exit(1);
+    } else {
+        // Parent process
+        if (is_background_process == 0) {
+            wait(NULL);
+        } else {
+            printf("[Background PID] %d\n", pid);
+        }
+    }
+}
+
+int main() {
     signal(SIGINT, handle_sigint);
+
     char input[MAX_BUFFER_SIZE];
     char *tokens[MAX_TOKENS];
     char exit_sequence[] = "exit";
-    // int tokenCount = 0;
-    
-    while(1){
-        int is_background_process = 0;
-        
-        printf("%s", "carapace> ");
 
-        if(fgets(input, sizeof(input), stdin) != NULL){
-            // strcspn will return the first index of the second argument in the first argument
-            input[strcspn(input, "\n")] = '\0';
-            if(strcmp(input, exit_sequence) == 0){
+    while (1) {
+        int is_background_process = 0;
+
+        print_prompt();
+
+        if (fgets(input, sizeof(input), stdin) != NULL) {
+            if (strcmp(input, "exit\n") == 0) {
                 printf("Bye!\n");
                 break;
             }
-            int tokenCount = 0;
-            char *word = strtok(input, " ");
-            while(word != NULL && tokenCount < MAX_TOKENS){
-                tokens[tokenCount] = word;
-                tokenCount++;
-                word = strtok(NULL, " ");
+            int tokenCount = parse_input(input, tokens, &is_background_process);
+            if (tokenCount == 0) {
+                continue;  // No command entered, skip
             }
-            tokens[tokenCount] = NULL;
-            if(strcmp(tokens[tokenCount-1], "&") == 0){
-                is_background_process = 1;
-                tokens[tokenCount - 1] = NULL;
-                tokenCount--;
-            }
-
-            pid_t pid = fork();
-            if(pid == -1){
-                perror("Fork failed:");
-                return 1;
-            } else if(pid == 0){
-                // child
-                if (execvp(tokens[0], tokens) == -1) {
-                    // execvp returns only if there is an error
-                    perror("execvp failed");
-                    return 1;
-                }
-            } else {
-                if(is_background_process == 0){
-                    wait(NULL);
-                } else{
-                    printf("[Background PID] %d\n", pid);
-                }
-            }
+            execute_command(tokens, is_background_process);
         } else {
-            printf(" Error reading input!");
+            printf("Error reading input!\n");
         }
     }
+
     return 0;
 }
